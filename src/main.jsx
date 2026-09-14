01190539a2c6b8054470fe747e4e1f8f8ff89a4c
Siam24857 <siamtechofficial1597@gmail.com>
2026-09-14 18:23:49 +0600

    all is done

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import About from "./pages/about";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
  
 
